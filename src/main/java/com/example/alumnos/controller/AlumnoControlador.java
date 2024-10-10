f08823518fdc2f1e841d2797bea21789c3efccda
EmiroMunoz01 <emiro_2001@icloud.com>
2024-10-09 21:48:59 -0500

    Se ha implementado el ResponseEntity para controlar la respuesta de la solicitud http, de momento es el codigo 200 ok

--- src/main/java/com/example/alumnos/controller/AlumnoControlador.java
+++ src/main/java/com/example/alumnos/controller/AlumnoControlador.java
@@ -2,8 +2,8 @@ package com.example.alumnos.controller;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,8 +24,9 @@ public class AlumnoControlador {
 
     // Obtener alumno
     @GetMapping({ "", "/" })
-    public List<Alumnos> getAll() {
-        return repository.findAll();
+    public ResponseEntity<List<Alumnos>> getAll() {
+        //return repository.findAll();
+        return ResponseEntity.ok(repository.findAll());
     }
 
     // Crear Alumno
